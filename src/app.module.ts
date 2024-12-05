7b6fe4c62711c9e83cc5768c9921a27b0cdab12a
eldanielsoares <danielsoares7717@gmail.com>
2024-12-05 12:19:12 -0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { UserCardModule } from './modules/user-card/user-card.module';
+import { MatchModule } from './modules/match/match.module';
 @Module({
-  imports: [UserModule, UserCardModule],
+  imports: [UserModule, UserCardModule, MatchModule],
   controllers: [],
   providers: [],
 })
